17a864e51279eb21942659f68414139721310fda
David Munoz <7dmunoz@gmail.com>
2022-09-27 17:14:12 -0700

    Fix console log bug

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // Get swagger doc URL from $text in changed file
-
-  // Navigate to URL and get swagger doc text
-
-  // Save swagger doc text to file
-
-  // Call validator
+  console.log('hello');
 } catch (error) {
   core.setFailed(error.message);
 }
