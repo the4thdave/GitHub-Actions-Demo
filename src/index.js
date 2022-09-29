a9a48a6faea5fed338d828554e61b51b35ef273d
David Munoz <7dmunoz@gmail.com>
2022-09-29 10:07:52 -0700

    Update github action behavior

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs');
 
-try {
-  console.log('hello');
-} catch (error) {
-  core.setFailed(error.message);
-}
+const readFileAndGetUrl = (inputFilePath) => {
+  const data = fs.readFileSync(inputFilePath, { encoding: 'utf-8', flag: 'r' });
+  const lines = data.split('\n');
+
+  for (let i = lines.length - 1; i >= 0; i--) {
+    let line = lines[i].trim();
+    if (line.startsWith('$text')) {
+      const url = line.substring(6);
+      return url.trim();
+    }
+  }
+};
+
+const converUrlForReq = (url) => {
+  let transformedUrl = url.replace('github.gapinc.com', 'github.gapin.com/raw');
+  transformedUrl += '?token=';
+  return transformedUrl;
+};
+
+const main = () => {
+  try {
+    const filesChanged = core.getInput('files-changed');
+    const url = readFileAndGetUrl(filesChanged);
+    const transformedUrl = converUrlForReq(url);
+    core.setOutput('raw-url', transformedUrl);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+main();
