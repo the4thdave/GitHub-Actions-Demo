4dd8a990eb9f35952af362fb472c1e592dccbd85
David Munoz <7dmunoz@gmail.com>
2022-09-29 10:17:20 -0700

    Fix function name

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const readFileAndGetUrl = (inputFilePath) => {
   }
 };
 
-const converUrlForReq = (url) => {
+const convertUrlForReq = (url) => {
   let transformedUrl = url.replace('github.gapinc.com', 'github.gapin.com/raw');
   transformedUrl += '?token=';
   return transformedUrl;
@@ -24,7 +24,7 @@ const main = () => {
   try {
     const filesChanged = core.getInput('files-changed');
     const url = readFileAndGetUrl(filesChanged);
-    const transformedUrl = converUrlForReq(url);
+    const transformedUrl = convertUrlForReq(url);
     core.setOutput('raw-url', transformedUrl);
   } catch (error) {
     core.setFailed(error.message);
